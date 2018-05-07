992e819e80a0b31f22dbc6be8d8a30c31b244545
liang3336 <632944695@qq.com>
2018-05-08 01:11:55 +0800

    update

--- app/src/main/java/sg/edu/rp/c346/dontstop/HomeActivity.java
+++ app/src/main/java/sg/edu/rp/c346/dontstop/HomeActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,9 +17,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private FirebaseUser user;
+    ImageView ivProfile;
+    TextView tvUserName;
+    NavigationView navigationView;
+    View headerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,22 @@ public class HomeActivity extends AppCompatActivity
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.nav_view);
+        headerView = navigationView.getHeaderView(0);
+        tvUserName = headerView.findViewById(R.id.textViewUsername);
+        ivProfile = headerView.findViewById(R.id.imageViewProfilePic);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String uid = user.getUid();
+            DocumentReference myRef = FirebaseFirestore.getInstance().document(uid + "/Profile");
+            String userName = user.getDisplayName();
+            String url = user.getPhotoUrl().toString();
+            Log.d("tag", url);
+            tvUserName.setText(userName);
+            Picasso.get().load(url).into(ivProfile);
+        }
 
         Fragment fragment = new HomeFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
